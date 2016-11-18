273205f78c14eafc9d991ed625ba13eeb3ec6226
Lukas Rademacher <lukas@rademacher.ac>
2016-11-18 12:23:01 +0100

    Fix for #3
    Should fix the responsive resize of the new dokuwiki templates described in #3

--- syntax.php
+++ syntax.php
@@ -70,7 +70,7 @@ class syntax_plugin_imagebox extends DokuWiki_Syntax_Plugin {
 
 			switch($state){
 				case DOKU_LEXER_ENTER:
-					$renderer->doc.= '<div class="thumb2 t'.$match['align'].'" style="width:'.($match['w']?($match['w']+10).'px':'auto').'"><div class="thumbinner">';
+					$renderer->doc.= '<div class="thumb2 t'.$match['align'].'"><div class="thumbinner">';
 					if($match['exist'])
 						$renderer->{$match['type']}($match['src'],$match['title'],'box2',$match['width'],$match['height'],$match['cache'],$match['linking']);
 					else
