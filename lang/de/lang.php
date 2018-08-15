2a1caa94f23bd3f843c9c03682d5ba45cc0ee46d
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-08-15 17:19:11 +1000

    Update lang.php

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,2 +1,8 @@
 <?php
-$lang['enlarge'] = 'vergrößern';
+/**
+ * German language file for the imagebox plugin
+ *
+ * @author FFTiger / myst6re <myst6re@wikisquare.com>
+ */
+
+$lang['enlarge'] = 'Vergrößern';
