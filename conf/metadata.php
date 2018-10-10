6905f6c6b0248916d0e604fccf2b1fbe9933de2f
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-10-10 23:17:34 +1000

    Update metadata.php

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Metadata for imagebox plugin
+ *
+ * @author FFTiger / myst6re <myst6re@wikisquare.com>
+ */
 $meta['display_magnify'] = array('multichoice','_choices' => array('If necessary','Always','Never'));
-$meta['center_align'] = array('onoff');
 $meta['default_caption_style'] = array('multichoice','_choices' => array('Normal','Italic','Bold'));
+$meta['center_align'] = array('onoff');
