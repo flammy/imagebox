4521a1d16fb7ad7691851ae78798f4bd2e4faff2
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-10-10 23:55:18 +1000

    Create settings.php

---
+++ lang/it/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Italian settings file for imagebox plugin
+ *
+ * @author Rainbow_Spike rainbow_spike@derpy.ru
+ */
+// keys need to match the config setting name
+$lang['display_magnify']       = 'Abilita la visualizzazione, l'icona con la lente d'ingrandimento';
+$lang['default_caption_style'] = 'Di Default lo stile della firma';
+$lang['center_align']          = 'Consentire la firma di centraggio';
