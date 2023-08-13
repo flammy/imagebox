c6830e8eec0fa0d089215a71fcbf40d15ad9006e
Lukas Rademacher <lukas@rademacher.ac>
2023-08-13 12:21:13 +0200

    fixes: check empty arrays

--- syntax.php
+++ syntax.php
@@ -126,7 +126,7 @@ class syntax_plugin_imagebox extends DokuWiki_Syntax_Plugin {
 					else
 						$renderer->doc.= 'Invalid Link';
 					$renderer->doc.= '<div class="thumbcaption" style="max-width: '.($match['width']-6).'px">';
-					if($match['detail']) {
+					if(isset($match['detail'])) {
 						$renderer->doc.= '<div class="magnify">';
 						$renderer->doc.= '<a class="internal" title="'.$this->getLang('enlarge').'" href="'.$match['detail'].'" target="_blank">';
 						$renderer->doc.= '<img width="15" height="11" alt="" src="'.DOKU_BASE.'lib/plugins/imagebox/magnify-clip.png"/>';
